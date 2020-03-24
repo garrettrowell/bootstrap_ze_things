aa5b881b30e409c29d1400bbdacf228485920913
Garrett Rowell <garrett.rowell@puppet.com>
2020-03-24 18:41:58 -0400

    start by copying gitlab commands

---
+++ tasks/install_gitlab.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# start by copying the commands from https://about.gitlab.com/install/#centos-7
+
+# 1. Install and configure the necessary dependencies
+yum install -y curl policycoreutils-python openssh-server
+systemctl enable sshd
+systemctl start sshd
+firewall-cmd --permanent --add-service=http
+firewall-cmd --permanent --add-service=https
+systemctl reload firewalld
+
+# 2. Add the GitLab package repository and install the package
+curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.rpm.sh | sudo bash
+
+EXTERNAL_URL="https://$(hostname)" yum install -y gitlab-ee
