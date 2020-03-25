e2edeae0088794fe81c6bb02f657e62683a40d7d
Garrett Rowell <garrett.rowell@puppet.com>
2020-03-25 17:20:21 -0400

    complete bootstrap gitlab plan

--- plans/bootstrap_gitlab.pp
+++ plans/bootstrap_gitlab.pp
@@ -3,12 +3,32 @@ plan bootstrap_ze_things::bootstrap_gitlab (
   String[1] $user,
   String[1] $password,
   String[1] $namespace,
+  String[1] $repo_name,
+  String[1] $import_url,
 ) {
-  $gitlab_name = get_targets($targets).map |$n| { $n.name }
-  $token_result = run_task('bootstrap_ze_things::gitlab_auth_token', $targets, host    => $gitlab_name, user => $user, password => $password)
-  $create_result = run_task('bootstrap_ze_things::gitlab_create_group', $targets, host => $gitlab_name, group_name => $namespace, group_path => $namespace, access_token => $token_result.to_data['result']['access_token'])
-  #  bootstrap_ze_things::gitlab_auth_token       Request a gitlab auth token
-  #bootstrap_ze_things::gitlab_create_group     Create a gitlab group
-  #bootstrap_ze_things::gitlab_project_import   Create a gitlab group
-  #bootstrap_ze_things::install_gitlab          A short description of this task
+  $gitlab_names = get_targets($targets).map |$n| { $n.name }
+
+  $gitlab_names.each |$gitlab_node| {
+    # install gitlab
+    run_task('bootstrap_ze_things::gitlab_install' $targets, user => $user, password => $password)
+
+    # retrieve the auth token for the user
+    $atr = run_task('bootstrap_ze_things::gitlab_auth_token',
+      $targets, host => $gitlab_node, user => $user, password => $password)
+
+    $access_token = $atr.to_data[0]['result']['access_token']
+
+    # assume the group doesn't exist so create it
+    $cgr = run_task('bootstrap_ze_things::gitlab_create_group',
+      $targets, host => $gitlab_node, group_name => $namespace, group_path => $namespace,
+      access_token => $access_token)
+
+    $namespace_id = $cgr.to_data[0]['result']['id']
+
+    # import the specified repo into the newly created group
+    run_task('bootstrap_ze_things::gitlab_project_import',
+      $targets, host => $gitlab_node, repo_name => $repo_name, import_url => $import_url,
+      namespace_id => $namespace_id, access_token => $access_token)
+  }
+
 }
