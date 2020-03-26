ca70eed80acd8c939aa54ec35e5cf55fd692136f
Garrett Rowell <garrett.rowell@puppet.com>
2020-03-26 14:13:55 -0400

    add gitlab add sshkey task

---
+++ tasks/gitlab_add_sshkey.rb
@@ -0,0 +1,31 @@
+#!/opt/puppetlabs/puppet/bin/ruby
+
+require "net/https"
+require "uri"
+require "json"
+
+begin
+  params = JSON.parse(STDIN.read)
+  result = Hash.new
+  uri = URI.parse("https://#{params['host']}/api/v4/user/keys")
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  headers = {
+    'Content-Type'  => 'application/json',
+    'Authorization' => "Bearer #{params['access_token']}",
+  }
+  data = {
+    'title' => params['title'],
+    'key'   => params['key'],
+  }
+  response = http.post(uri.path, data.to_json, headers)
+  result = JSON.parse(response.body)
+rescue Exception => e
+  result[:_error] = { msg: e.message,
+                      kind: "puppetlabs-example_modules/unknown",
+                      details: { class: e.class.to_s },
+  }
+end
+
+puts result.to_json
