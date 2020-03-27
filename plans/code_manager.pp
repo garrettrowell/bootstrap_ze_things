0bbc6cb856e30fcec4fd2914eb74647afbf04631
Garrett Rowell <garrett.rowell@puppet.com>
2020-03-27 10:57:38 -0400

    gitlab_install modified. add node_group to code_manager plan for classification

--- plans/code_manager.pp
+++ plans/code_manager.pp
@@ -1,28 +1,50 @@
 plan bootstrap_ze_things::code_manager (
   TargetSpec $puppetmaster,
   TargetSpec $gitlab,
-  String[1]  $ssh_comment     = 'PE Code Manager',
+  String[1]  $ssh_comment     = 'PE_Code_Manager',
   String     $ssh_password    = '',
   String[1]  $ssh_keyfile     = '/etc/puppetlabs/puppetserver/ssh/id-control_repo.rsa',
   String[1]  $gitlab_user     = 'root',
   String[1]  $gitlab_password = 'puppetlabs',
   String[1]  $gitlab_host,
 ) {
-    # generate key-pair on puppetmaster
-    $skr = run_task('bootstrap_ze_things::ssh_keygen',
-                    $puppetmaster, keyfile => $ssh_keyfile, password => $ssh_password, comment => $ssh_comment)
 
-    $sshkey = $skr.to_data[0]['result']['public_key']
+  # generate key-pair on puppetmaster
+  $skr = run_task('bootstrap_ze_things::ssh_keygen', $puppetmaster, keyfile => $ssh_keyfile, password => $ssh_password, comment => $ssh_comment)
 
-    # retrieve the auth token for the user
-    $atr = run_task('bootstrap_ze_things::gitlab_auth_token',
-                    $gitlab, host => $gitlab_host, user => $gitlab_user, password => $gitlab_password)
+  $sshkey = $skr.to_data[0]['result']['public_key']
 
-    $access_token = $atr.to_data[0]['result']['access_token']
+  # retrieve the auth token for the user
+  $atr = run_task('bootstrap_ze_things::gitlab_auth_token', $gitlab, host => $gitlab_host, user => $gitlab_user, password => $gitlab_password)
 
-    # add the sshkey to the users account
-    run_task('bootstrap_ze_things::gitlab_add_sshkey',
-             $gitlab, host => $gitlab_host, access_token => $access_token, title => $ssh_comment, key => $sshkey)
+  $access_token = $atr.to_data[0]['result']['access_token']
+
+  # add the sshkey to the users account
+  run_task('bootstrap_ze_things::gitlab_add_sshkey', $gitlab, host => $gitlab_host, access_token => $access_token, title => $ssh_comment, key => $sshkey)
+
+  # Do some hacky bs to get node_group to work on master
+  $puppetmaster.apply_prep
+  apply($puppetmaster) {
+
+    file { 'classifier.yaml':
+      ensure  => file,
+      mode    => '0644',
+      path    => Deferred('bootstrap_ze_things::node_manager_config_path'),
+      content => epp('bootstrap_ze_things/node_manager/classifier.yaml.epp', {
+        server => 'master.puppetdebug.vlan',
+        }),
+    }
 
 
+    node_group { 'PE Master':
+      data    => {
+        'puppet_enterprise::profile::master' =>  {
+          'code_manager_auto_configure' => true,
+          'r10k_private_key'            => '/etc/puppetlabs/puppetserver/ssh/id-control_repo.rsa',
+          'r10k_remote'                 => 'git@gitlab.puppetdebug.vlan:puppet/control-repo.git',
+        }
+      },
+      require => File['classifier.yaml'],
+    }
+  }
 }
--- tasks/gitlab_install.sh
+++ tasks/gitlab_install.sh
@@ -14,3 +14,14 @@ systemctl reload firewalld
 curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.rpm.sh | sudo bash
 
 EXTERNAL_URL="https://${PT_host}" GITLAB_ROOT_PASSWORD=$PT_password yum install -y gitlab-ee
+
+# Disable exit on non 0
+set +e
+
+# added to ensure /var/opt/gitlab/.ssh gets correct security context
+# I've found this likes to fail due to letsencrypt if you throw it a bogus
+# fqdn. well be lazy for now and ignore any potential errors
+gitlab-ctl reconfigure
+
+# Enable exit on non 0
+set -e
